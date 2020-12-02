0bf4b5e52cff08415f3be00eeb20030d6e29db21
Yiran Li <yiran.li@hotmail.com>
2020-12-01 21:22:29 -0700

    Add handleClick and displayAllMovies methods

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
 import movieData from './components/movieData';
-import Movies from './components/Movies'
+import Movies from './components/Movies';
+import ChosenMovie from './components/ChosenMovie';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      movies: movieData.movies
+      movies: movieData.movies,
+      chosenMovie: null
     }
+  }
+
+  handleClick = (id) => {
+    const chosenMovie = this.state.movies.find(movie => movie.id === id)
+    this.setState({
+      chosenMovie: chosenMovie
+    })
+  }
+
+  displayAllMovies = (event) => {
+    event.preventDefault();
+    this.setState({
+      chosenMovie: null
+    })
   }
 
   render() {
     return (
       <main className='App'>
-        <Movies movies={this.state.movies}/>
+        {this.state.chosenMovie && 
+          <ChosenMovie 
+            movie={this.state.chosenMovie}
+            displayAllMovies={this.displayAllMovies}
+          />}
+        {!this.state.chosenMovie && 
+          <Movies 
+            movies={this.state.movies}
+            handleClick={this.handleClick}
+          />
+        }
       </main>
     )
   }
