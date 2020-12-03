77ce8f43f0865824a1b3bb14594916fc3c3a3999
Cooper Terrones <cooper.terrones@outlook.com>
2020-12-02 22:20:49 -0700

    Add iFrame positioning

--- src/components/ChosenMovie.js
+++ src/components/ChosenMovie.js
@@ -13,25 +13,25 @@ const ChosenMovie = ({movie, video, displayAllMovies}) => {
   return (
     <section className='chosen-movie'>
       <section className='backdrop-img'style={{ backgroundImage: `linear-gradient(to right, black, 55%, transparent), url(${movie.backdrop_path})`}}>
-      <section className="where-r-u">
-        <h3>{movie.title}</h3>
-      <section className='numeric-info'>
-        <p>Rating: {movie.average_rating.toFixed(1)}</p>
-        <p>Runtime: {movie.runtime}</p>
-        <p>Release Date: {movie.release_date}</p>
-      </section>
-      <p>Overview: {movie.overview}</p>
-      <section className='genre'>{genres}</section>
-      <section className='monetary-info'>
-        <p>Budget: ${movie.budget}</p>
-        <p>Revenue: ${movie.revenue}</p>
-      </section>
-      <p>Tagline: {movie.tagline}</p>
-      </section>
-      <section className="video-frame">
-        <iframe src={`https://youtube.com/embed/${video.key}`} height="200" width="300"></iframe>
-      </section>
-      <button onClick={displayAllMovies}>Go Back</button>
+        <button onClick={displayAllMovies}>Go Back</button>
+        <section className="movie-info">
+          <h3>{movie.title}</h3>
+        <section className='numeric-info'>
+          <p>Rating: {movie.average_rating.toFixed(1)}</p>
+          <p>Runtime: {movie.runtime}</p>
+          <p>Release Date: {movie.release_date}</p>
+        </section>
+        <p>Overview: {movie.overview}</p>
+        <section className='genre'>{genres}</section>
+        <section className='monetary-info'>
+          <p>Budget: ${movie.budget}</p>
+          <p>Revenue: ${movie.revenue}</p>
+        </section>
+        <p>Tagline: {movie.tagline}</p>
+        </section>
+        <section className="video-frame">
+          <iframe src={`https://youtube.com/embed/${video.key}`} height="400" width="700"></iframe>
+        </section>
       </section>
     </section>
   )
