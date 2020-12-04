4ea3e124e0adb19d5ceb522e523cc1732806f849
Yiran Li <yiran.li@hotmail.com>
2020-12-03 17:17:14 -0700

    Add test file for ChosenMovie component

---
+++ src/test/ChosenMovie.test.js
@@ -0,0 +1,9 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ChosenMovie from '../components/ChosenMovie';
+
+describe('ChosenMovie Component', () => {
+  it('should render correctly', () => {
+
+  })
+})
