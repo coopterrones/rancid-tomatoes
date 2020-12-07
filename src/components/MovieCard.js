86dcc15cc483451c9fdd2ad1e6e67d2162e2946a
Yiran Li <yiran.li@hotmail.com>
2020-12-07 16:27:04 -0700

    Add navlink to this component

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import '../styles/MovieCard.scss';
+import { NavLink } from 'react-router-dom';
 
 const MovieCard = (props) => {
-  const {id, title, avgRating, releaseDate, posterImg, handleClick} = props;
-
+  const {id, title, avgRating, releaseDate, posterImg} = props;
   return (
-    <section data-testid={`movie-card-${id}`} className='movie-card'id={id} onClick={() => {handleClick(id)}}>
-      <section>
-        <img src={posterImg} className='poster-img' alt={title}/>
+    <NavLink to={`/movie/${id}`} className='movie-card'>
+      <section data-testid={`movie-card-${id}`} id={id}>
+        <section>
+          <img src={posterImg} className='poster-img' alt={title}/>
+        </section>
+        <section>
+          <h3>{title}</h3>
+          <p>Rating: {avgRating}</p>
+          <p>Release Date: {releaseDate}</p>
+        </section>
       </section>
-      <section>
-        <h3>{title}</h3>
-        <p>Rating: {avgRating}</p>
-        <p>Release Date: {releaseDate}</p>
-      </section>
-    </section>
+    </NavLink>
   )
 }
 
