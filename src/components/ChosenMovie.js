05d427513a242d46510aebff14dc4aff172ec19e
Cooper Terrones <cooper.terrones@outlook.com>
2020-12-08 16:45:59 -0700

    Add styling updates

--- src/components/ChosenMovie.js
+++ src/components/ChosenMovie.js
@@ -5,13 +5,14 @@ import ratingStar from '../assets/rating-star.svg';
 import runtimeReel from '../assets/runtime-reel.svg';
 import { apiCalls } from '../apiCalls';
 import { Link } from 'react-router-dom';
+import { Spring, Transition } from 'react-spring/renderprops'
 
 class ChosenMovie extends Component {
   constructor() {
     super();
     this.state = {
       movie: null,
-      video: null
+      video: null,
     }
   }
  
@@ -38,7 +39,7 @@ class ChosenMovie extends Component {
         <section className='backdrop-img'style={{ backgroundImage: `linear-gradient(to right, black, 55%, transparent), url(${movie.backdrop_path})`}}>
           <section className="movie-info">
             <Link to='/'>
-              <img data-testid='return-btn' className="back-button-icon" src={backButton} alt='back-button-icon'onClick={displayAllMovies}/>
+              <img data-testid='return-btn' className="back-button-icon" src={backButton} alt='back-button-icon'/>
             </Link>
             <h3>{movie.title}</h3>
           <section className='numeric-info'>
@@ -54,9 +55,17 @@ class ChosenMovie extends Component {
           </section>
           <p>Tagline: {movie.tagline}</p>
           </section>
-          <section className="video-frame">
-            <iframe className="video-container"src={`https://youtube.com/embed/${video.key}`} alt='trailer-iframe-video-player' title='trailer-video-player' height="400" width="700"></iframe>
+          <Spring
+            config={{mass:2, tension: 6.5, friction: 0, clamp: true}}
+            from={{opactiy: 0, marginTop: -250}}
+            to={{opactiy: 1, marginTop: 0}}
+          >
+            { props => (  
+              <section className="video-frame" style={ props }>
+                <iframe className="video-container"src={`https://youtube.com/embed/${video.key}`} alt='trailer-iframe-video-player' title='trailer-video-player' height="400" width="700"></iframe>                
               </section>
+            )} 
+          </Spring>
           </section>
         </section>
       )
