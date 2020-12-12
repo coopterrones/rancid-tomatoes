b452d3dc70a4098a1596a97eb46ae8136ad9644e
Yiran Li <yiran.li@hotmail.com>
2020-12-11 20:29:00 -0700

    Add and pass addToWatchList method

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Movies from './components/Movies';
 import ChosenMovie from './components/ChosenMovie';
-import {apiCalls} from './apiCalls';
-import { Route, Switch } from 'react-router-dom';
+import { apiCalls } from './apiCalls';
+import { Route } from 'react-router-dom';
 import Loading from './components/Loading';
 import Navigation from './components/Navigation';
 import Error from './components/Error';
@@ -25,15 +25,15 @@ class App extends Component {
 
   getData = () => {
     apiCalls.allMovies()
-    .then(data => {
+      .then(data => {
         this.setState({
           movies: data.movies,
           loaded: true
         })
       })
-    .catch(err => this.setState({
-      error: err.message
-    }))
+      .catch(err => this.setState({
+        error: err.message
+      }))
   }
 
   getSortedMovies = (sortedMovies) => {
@@ -42,23 +42,34 @@ class App extends Component {
     })
   }
 
+  addToWatchList = (id) => {
+    apiCalls.addToWatchList(id);
+  }
+
   render() {
     const { movies, queries, error, loaded } = this.state;
     const displayMovies = queries.length ? queries : movies;
     return (
       <main className='App'>
         {error && <Error />}
-        {!loaded && <Loading/> }
-          <Route path='/' exact 
-            render={() => 
-              <Navigation movies={displayMovies} getSortedMovies={this.getSortedMovies}/> 
-          }/>
+        {!loaded && <Loading />}
+        <Route path='/' exact
+          render={() =>
+            <Navigation
+              movies={displayMovies}
+              getSortedMovies={this.getSortedMovies}
+            />
+          } />
 
-          <Route path='/' exact 
-            render={ () => <Movies movies={displayMovies} />
-          }/>
-          
-          <Route path='/movie/:id' component={ ChosenMovie } /> 
+        <Route path='/' exact
+          render={() =>
+            <Movies
+              movies={displayMovies}
+              addToWatchList={this.addToWatchList}
+            />
+          } />
+
+        <Route path='/movie/:id' component={ChosenMovie} />
       </main>
     )
   }
