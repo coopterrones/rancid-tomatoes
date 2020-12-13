84a3fda1bab77573457a2a86ef94382d7da7d419
Yiran Li <yiran.li@hotmail.com>
2020-12-12 17:24:40 -0700

    Update test, sepearte mock data

--- src/test/Movies.test.js
+++ src/test/Movies.test.js
@@ -1,40 +1,26 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { _movies } from './mockData/moviesMockData';
 import '@testing-library/jest-dom';
 import Movies from '../components/Movies';
-import { MemoryRouter} from 'react-router-dom'
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Movies Component', () => {
   it('should render correctly', () => {
     render(
       <Movies
-        movies={[
-        {
-          id: 694919,
-          title:"Money Plane",
-          release_date:"2020-09-29",
-          poster_path:"https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-          average_rating:7
-        },
-        {
-          id: 694456,
-          title:"Elle's Money Plane",
-          release_date:"2020-10-31",
-          poster_path:"https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-          average_rating:10
-        }
-        ]}
-      />, {wrapper: MemoryRouter}
+        movies={_movies}
+      />, { wrapper: MemoryRouter }
     )
 
     const title1 = screen.getByText('Money Plane');
-    const releaseDate1 = screen.getByText('Release Date: 2020-09-29');
-    const averageRating1 = screen.getByText('Rating: 10.0');
+    const releaseDate1 = screen.getByText('Sep 28, 2020');
+    const averageRating1 = screen.getByText('7.0');
     const posterImages = screen.getAllByRole('img');
 
     expect(title1).toBeInTheDocument();
     expect(releaseDate1).toBeInTheDocument();
     expect(averageRating1).toBeInTheDocument();
-    expect(posterImages).toHaveLength(2);
+    expect(posterImages).toHaveLength(6);
 
   })
 })
