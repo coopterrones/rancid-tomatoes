55c079e7f9bd42d06dce7c1c83598e5141acb423
Cooper Terrones <cooper.terrones@outlook.com>
2020-12-14 13:23:37 -0700

    app test fixes

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,67 +1,71 @@
-import {render, screen, waitFor, fireEvent, getByTestId} from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import App from '../App';
 import { apiCalls } from '../apiCalls';
 import { _movies } from './mockData/appMockData';
-import { Router } from 'react-router-dom';
+import { Router, MemoryRouter } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 jest.mock('../apiCalls');
 
 describe("App Component", () => {
-  
   beforeEach(() => {
     apiCalls.allMovies.mockResolvedValueOnce(_movies);
+    apiCalls.getWatchList.mockResolvedValueOnce({ "watchListIds": [] });
   });
-
   describe('getData', () => {
-
     it('should call allMovies', () => {
       const history = createMemoryHistory();
       render(<Router history={history}><App /></Router>)
-
       expect(apiCalls.allMovies).toHaveBeenCalledTimes(1);
     })
-
+    it('should render loading', () => {
+      const history = createMemoryHistory();
+      render(<Router history={history}><App /></Router>)
+      const loadingMessage = screen.getByText('Loading...');
+      expect(loadingMessage).toBeInTheDocument();
+    })
+    it('should load all movies', async () => {
+      const history = createMemoryHistory();
+      render(<Router history={history}><App /></Router>)
+      const mockTitle1 = await waitFor(() => screen.getByText('Money Plane'));
+      const mockImgs1 = await waitFor(() => screen.getAllByRole('img'));
+      const mockAvgRating1 = await waitFor(() => screen.getByText('6.7'));
+      const mockReleaseDate1 = await waitFor(() => screen.getByText('Aug 19, 2020'));
+      expect(mockTitle1).toBeInTheDocument();
+      expect(mockImgs1).toHaveLength(10);
+      expect(mockAvgRating1).toBeInTheDocument();
+      expect(mockReleaseDate1).toBeInTheDocument();
+    })
+    it('should load correct url', async () => {
+      const history = createMemoryHistory();
+      render(<Router history={history}><App /></Router>);
+      expect(history.location.pathname).toBe("/");
+    })
   })
-
-  it('should render loading', () => {
-    const history = createMemoryHistory();
-    render(<Router history={history}><App /></Router>)
-
-    const loadingMessage = screen.getByText('Loading page...');
-
-    expect(loadingMessage).toBeInTheDocument();
+  describe('Nav section', () => {
+    it('should display correct movie upon searching', async () => {
+      render(<App />, { wrapper: MemoryRouter });
+      const searchInput = await waitFor(() => screen.getByPlaceholderText('MOVIE NAME'));
+      const submitBtn = await waitFor(() => screen.getAllByRole('button')[0]);
+      userEvent.type(searchInput, 'Mulan');
+      userEvent.click(submitBtn);
+      const title = await waitFor(() => screen.getByText('Mulan'));
+      const rating = await waitFor(() => screen.getByText(4.9))
+      const releaseDate = await waitFor(() => screen.getByText('Sep 03, 2020'))
+      expect(title).toBeInTheDocument();
+      expect(rating).toBeInTheDocument();
+      expect(releaseDate).toBeInTheDocument();
+    })
   })
+  describe('Chosen movie', () => {
+    it('should redirect upon clicking on movie', async () => {
+      const history = createMemoryHistory();
+      render(<Router history={history}><App /></Router>);
+      const mockImg = await waitFor(() => screen.getByAltText('Money Plane'));
+      userEvent.click(mockImg)
+      expect(history.location.pathname).toEqual('/movie/694919');
 
-  it('should load all movies', async() => {
-    const history = createMemoryHistory();
-    render(<Router history={history}><App /></Router>)
-
-    const mockTitle1 = await waitFor(() => screen.getByText('Money Plane'));
-    const mockImgs1 = await waitFor(() => screen.getAllByRole('img'));
-    const mockAvgRating1 = await waitFor(() => screen.getByText('Rating: 6.7'));
-    const mockReleaseDate1 = await waitFor(() => screen.getByText('Release Date: 2020-09-29'));
-
-    expect(mockTitle1).toBeInTheDocument();
-    expect(mockImgs1).toHaveLength(3);
-    expect(mockAvgRating1).toBeInTheDocument();
-    expect(mockReleaseDate1).toBeInTheDocument();
-  })
-
-  it('should load correct url', async() => {
-    const history = createMemoryHistory();
-    render(<Router history={history}><App /></Router>);
- 
-    expect(history.location.pathname).toBe("/");
-  })
-
-  it('should redirect upon clicking on movie', async() => {
-    const history = createMemoryHistory();
-    render(<Router history={history}><App /></Router>);
-
-    const mockTitle = await waitFor(() => screen.getByText('Money Plane'));
-    fireEvent.click(mockTitle)
-    
-    expect(history.location.pathname).toEqual('/movie/694919');
+    })
   })
 });
