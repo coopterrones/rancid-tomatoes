4bb01ec1e6bae87b87baa3628c3ab353892e1d29
Yiran Li <yiran.li@hotmail.com>
2020-12-16 14:11:27 -0700

    FIx bug display non when there is no video

--- src/components/ChosenMovie.js
+++ src/components/ChosenMovie.js
@@ -15,7 +15,7 @@ class ChosenMovie extends Component {
       video: null,
     }
   }
- 
+
   componentDidMount() {
     this.getChosenMovieData();
   }
@@ -25,8 +25,8 @@ class ChosenMovie extends Component {
     return Promise.all([apiCalls.selectMovie(id), apiCalls.selectVideo(id)])
       .then(data => {
         const chosenMovie = data.reduce((chosenMovieData, eachDataset) => {
-          return chosenMovieData = {...chosenMovieData, ...eachDataset}
-        }, {}) 
+          return chosenMovieData = { ...chosenMovieData, ...eachDataset }
+        }, {})
         this.setState({
           movie: chosenMovie.movie,
           video: chosenMovie.videos[0]
@@ -48,22 +48,22 @@ class ChosenMovie extends Component {
     }
   }
 
-  render() {  
-    if(this.state.movie) {
+  render() {
+    if (this.state.movie) {
       const { movie, video } = this.state;
       const genres = movie.genres.map(genre => <p key={genre}>{genre} </p>)
       const releaseDate = this.formatDate(movie.release_date);
       return (
         <section className='chosen-movie' data-testid='chosen-movie'>
-          <section className='backdrop-img'style={{ backgroundImage: `linear-gradient(to right, black, 55%, transparent), url(${movie.backdrop_path})`}}>
+          <section className='backdrop-img' style={{ backgroundImage: `linear-gradient(to right, black, 55%, transparent), url(${movie.backdrop_path})` }}>
             <section className="movie-info">
               <Link to='/'>
-                <img data-testid='return-btn' className="back-button-icon" src={backButton} alt='back-button-icon'/>
+                <img data-testid='return-btn' className="back-button-icon" src={backButton} alt='back-button-icon' />
               </Link>
               <h3>{movie.title}</h3>
               <section className='numeric-info'>
-                <p className='rating-text'><img className='rating-star' src={ratingStar} alt='rating-star-icon'/> {movie.average_rating.toFixed(1)}</p>
-                <p className='runtime-text'><img className='runtime-reel' src={runtimeReel} alt='runtime-reel-icon'/>{movie.runtime}min.</p>
+                <p className='rating-text'><img className='rating-star' src={ratingStar} alt='rating-star-icon' /> {movie.average_rating.toFixed(1)}</p>
+                <p className='runtime-text'><img className='runtime-reel' src={runtimeReel} alt='runtime-reel-icon' />{movie.runtime}min.</p>
                 <p className='release-date-text-chosen'>{releaseDate[1]} {releaseDate[2]}, {releaseDate[3]}</p>
               </section>
               <p className='overview-text' data-testid='overview'>Overview: {movie.overview}</p>
@@ -75,14 +75,14 @@ class ChosenMovie extends Component {
               <p>Tagline: {movie.tagline}</p>
             </section>
             <section className="video-frame">
-              <iframe className="video-container"src={`https://youtube.com/embed/${video.key}`} alt='trailer-iframe-video-player' title='trailer-video-player' height="400" width="700"></iframe>                
+              {video && <iframe className="video-container" src={`https://youtube.com/embed/${video.key}`} alt='trailer-iframe-video-player' title='trailer-video-player' height="400" width="700"></iframe>}
             </section>
           </section>
         </section>
       )
-   } else {
-    return (<Loading/>)
-   }
+    } else {
+      return (<Loading />)
+    }
   }
 }
 
