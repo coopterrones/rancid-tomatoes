9546735ae1829b2a5f1b5d184e4a75904c4fa6ca
Yiran Li <yiran.li@hotmail.com>
2020-12-20 14:36:48 -0700

    Change component from class to function

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,60 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import rancidTomato from '../assets/rancid-tomatillos.png';
 import '../styles/Navigation.scss';
 
-class Navigation extends Component {
-  constructor() {
-    super();
-    this.state = {
-      search: ''
-    }
+const Navigation = (props) => {
+  const [search, updateSearch] = useState('');
+  const { movies, getSearchedMovies, sortMovies, sortStatus } = props;
+
+  const handleClick = () => {
+    sortMovies();
   }
 
-  handleClick = () => {
-    this.props.sortMovies();
+  const handleOnChange = (event) => {
+    updateSearch(event.target.value.toLowerCase())
   }
 
-  handleOnChange = (event) => {
-    this.setState({
-      search: event.target.value.toLowerCase()
-    })
-  }
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const { search } = this.state;
-    const updateMovies = this.props.movies.filter(movie => movie.title.toLowerCase().includes(search))
-    this.setState({
-      search: ''
-    })
-    this.props.getSearchedMovies(updateMovies)
+    const updateMovies = movies.filter(movie => movie.title.toLowerCase().includes(search))
+    updateSearch('')
+    getSearchedMovies(updateMovies)
   }
 
-  render() {
-    const { getSearchedMovies, sortStatus } = this.props;
-    return (
-      <nav className='nav-bar'>
-        <section className="nav-top">
-          <img className='logo' src={rancidTomato} alt='logo' height='75px' width='225px' onClick={() => getSearchedMovies([])} />
-          <form onSubmit={this.handleSubmit} className='filter-inputs'>
-            <div className="search-input-wrap">
-              <input className="search-input" id="search" placeholder="MOVIE NAME" name="search" value={this.state.search} onChange={this.handleOnChange} />
-              <label htmlFor='search'></label>
-            </div>
-            <input className="search-button" type="submit" value='search' />
-          </form>
-        </section>
-        <section className="nav-bottom">
-          <button className='sort-button' onClick={this.handleClick}>{sortStatus ? "Oldest - Newest" : "Newest - Oldest"}</button>
-          <Link to='/watch-list'>
-            <button className='watch-list-button'>Watch List</button>
-          </Link>
-        </section>
-      </nav>
+  return (
+    <nav className='nav-bar'>
+      <section className="nav-top">
+        <img className='logo' src={rancidTomato} alt='logo' height='75px' width='225px' onClick={() => getSearchedMovies([])} />
+        <form onSubmit={handleSubmit} className='filter-inputs'>
+          <div className="search-input-wrap">
+            <input className="search-input" id="search" placeholder="MOVIE NAME" name="search" value={search} onChange={handleOnChange} />
+            <label htmlFor='search'></label>
+          </div>
+          <input className="search-button" type="submit" value='search' />
+        </form>
+      </section>
+      <section className="nav-bottom">
+        <button className='sort-button' onClick={handleClick}>{sortStatus ? "Oldest - Newest" : "Newest - Oldest"}</button>
+        <Link to='/watch-list'>
+          <button className='watch-list-button'>Watch List</button>
+        </Link>
+      </section>
+    </nav>
+  )
 
-    )
-  }
 }
 
 export default Navigation;
