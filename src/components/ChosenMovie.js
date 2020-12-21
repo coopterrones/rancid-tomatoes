dfb9c16178d4803c82ecdb65aa434070af971d10
Yiran Li <yiran.li@hotmail.com>
2020-12-21 13:35:49 -0700

    Clean up space

--- src/components/ChosenMovie.js
+++ src/components/ChosenMovie.js
@@ -13,63 +13,107 @@ const ChosenMovie = ({ match }) => {
 
   const getChosenMovieData = () => {
     const { id } = match.params;
-    return Promise.all([apiCalls.selectMovie(id), apiCalls.selectVideo(id)])
-      .then(data => {
-        const chosenMovie = data.reduce((chosenMovieData, eachDataset) => {
-          return chosenMovieData = { ...chosenMovieData, ...eachDataset }
-        }, {})
-        setMovie(chosenMovie.movie)
-        setVideo(chosenMovie.videos[0])
-      })
-  }
+    return Promise.all([
+      apiCalls.selectMovie(id),
+      apiCalls.selectVideo(id),
+    ]).then((data) => {
+      const chosenMovie = data.reduce((chosenMovieData, eachDataset) => {
+        return (chosenMovieData = { ...chosenMovieData, ...eachDataset });
+      }, {});
+      setMovie(chosenMovie.movie);
+      setVideo(chosenMovie.videos[0]);
+    });
+  };
 
   useEffect(() => getChosenMovieData(), []);
 
   const formatDate = (movieReleaseDate) => {
     const date = new Date(movieReleaseDate);
     return date.toDateString().split(' ');
-  }
+  };
 
   const formatMonetaryInfo = (amount) => {
     const formattedAmount = amount / 1000000;
     return amount % 1000000 ? formattedAmount.toFixed(2) : formattedAmount;
-  }
-
+  };
 
   if (movie) {
-    const genres = movie.genres.map(genre => <p key={genre}>{genre} </p>)
+    const genres = movie.genres.map((genre) => <p key={genre}>{genre} </p>);
     const releaseDate = formatDate(movie.release_date);
     return (
       <section className='chosen-movie' data-testid='chosen-movie'>
-        <section className='backdrop-img' style={{ backgroundImage: `linear-gradient(to right, black, 55%, transparent), url(${movie.backdrop_path})` }}>
-          <section className="movie-info">
+        <section
+          className='backdrop-img'
+          style={{
+            backgroundImage: `linear-gradient(to right, black, 55%, transparent), url(${movie.backdrop_path})`,
+          }}
+        >
+          <section className='movie-info'>
             <Link to='/'>
-              <img data-testid='return-btn' className="back-button-icon" src={backButton} alt='back-button-icon' />
+              <img
+                data-testid='return-btn'
+                className='back-button-icon'
+                src={backButton}
+                alt='back-button-icon'
+              />
             </Link>
             <h3>{movie.title}</h3>
             <section className='numeric-info'>
-              <p className='rating-text'><img className='rating-star' src={ratingStar} alt='rating-star-icon' /> {movie.average_rating.toFixed(1)}</p>
-              <p className='runtime-text'><img className='runtime-reel' src={runtimeReel} alt='runtime-reel-icon' />{movie.runtime}min.</p>
-              <p className='release-date-text-chosen'>{releaseDate[1]} {releaseDate[2]}, {releaseDate[3]}</p>
+              <p className='rating-text'>
+                <img
+                  className='rating-star'
+                  src={ratingStar}
+                  alt='rating-star-icon'
+                />{' '}
+                {movie.average_rating.toFixed(1)}
+              </p>
+              <p className='runtime-text'>
+                <img
+                  className='runtime-reel'
+                  src={runtimeReel}
+                  alt='runtime-reel-icon'
+                />
+                {movie.runtime}min.
+              </p>
+              <p className='release-date-text-chosen'>
+                {releaseDate[1]} {releaseDate[2]}, {releaseDate[3]}
+              </p>
             </section>
-            <p className='overview-text' data-testid='overview'>Overview: {movie.overview}</p>
-            <section className='genre' data-testid='genre'>{genres}</section>
+            <p className='overview-text' data-testid='overview'>
+              Overview: {movie.overview}
+            </p>
+            <section className='genre' data-testid='genre'>
+              {genres}
+            </section>
             <section className='monetary-info'>
-              <p className='budget-text'>Budget: ${formatMonetaryInfo(movie.budget)}M</p>
-              <p className='revenue-text'>Revenue: ${formatMonetaryInfo(movie.revenue)}M</p>
+              <p className='budget-text'>
+                Budget: ${formatMonetaryInfo(movie.budget)}M
+              </p>
+              <p className='revenue-text'>
+                Revenue: ${formatMonetaryInfo(movie.revenue)}M
+              </p>
             </section>
             <p>Tagline: {movie.tagline}</p>
           </section>
-          <section className="video-frame">
-            {video && <iframe className="video-container" src={`https://youtube.com/embed/${video.key}`} alt='trailer-iframe-video-player' title='trailer-video-player' height="400" width="700"></iframe>}
+          <section className='video-frame'>
+            {video && (
+              <iframe
+                className='video-container'
+                src={`https://youtube.com/embed/${video.key}`}
+                alt='trailer-iframe-video-player'
+                title='trailer-video-player'
+                height='400'
+                width='700'
+              ></iframe>
+            )}
           </section>
         </section>
       </section>
-    )
+    );
   } else {
-    return (<Loading />)
+    return <Loading />;
   }
-}
+};
 
 // class ChosenMovie extends Component {
 //   constructor() {
@@ -146,4 +190,4 @@ const ChosenMovie = ({ match }) => {
 //   }
 // }
 
-export default ChosenMovie
+export default ChosenMovie;
